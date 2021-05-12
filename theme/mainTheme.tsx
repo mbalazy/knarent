055db8f1b9b585d41d08d39a0ca1a30e409ba026
mbalazy <p@mbalazy.pl>
2021-05-12 18:49:21 +0200

    Refacton as const in mainTheme

--- theme/mainTheme.tsx
+++ theme/mainTheme.tsx
@@ -1,48 +1,48 @@
 const mainTheme = {
   colors: {
-    primary: '#FFD717' as const,
-    secondary: '#204AAB' as const,
-    accent1: '#083358' as const,
-    accent2: '#001F3F' as const,
-    accent3: '#0F358C' as const,
-    primaryText: '#222222' as const,
-    lightText: '#FFFFFF' as const,
-    white: '#FFFFFF' as const,
-    grey100: 'hsl(0, 0%, 90%)' as const,
-    grey002: 'hsl(0, 0%, 98%)' as const,
-    grey005: 'hsl(0, 0%, 95%)' as const,
-    grey850: 'hsl(0, 0%, 15%)' as const,
-    black: 'hsl(0, 0%, 0%)' as const,
+    primary: '#FFD717',
+    secondary: '#204AAB',
+    accent1: '#083358',
+    accent2: '#001F3F',
+    accent3: '#0F358C',
+    primaryText: '#222222',
+    lightText: '#FFFFFF',
+    white: '#FFFFFF',
+    grey100: 'hsl(0, 0%, 90%)',
+    grey002: 'hsl(0, 0%, 98%)',
+    grey005: 'hsl(0, 0%, 95%)',
+    grey850: 'hsl(0, 0%, 15%)',
+    black: 'hsl(0, 0%, 0%)',
   },
 
   fontSize: {
-    xs: '1.2rem' as const,
-    s: '1.5rem' as const,
-    m: '1.6rem' as const,
-    l: '1.8rem' as const,
-    xl: '2.2rem' as const,
-    '2xl': '2.4rem' as const,
-    '3xl': '3rem' as const,
+    xs: '1.2rem',
+    s: '1.5rem',
+    m: '1.6rem',
+    l: '1.8rem',
+    xl: '2.2rem',
+    '2xl': '2.4rem',
+    '3xl': '3rem',
   },
 
   fontWeight: {
-    light: 300 as const,
-    regular: 400 as const,
-    bold: 600 as const,
-    extrabold: 900 as const,
+    light: 300,
+    regular: 400,
+    bold: 600,
+    extrabold: 900,
   },
 
   border: {
-    standard: '20px' as const,
-    light: '15px' as const,
+    standard: '20px',
+    light: '15px',
   },
 
   dimensions: {
-    navbarHeight: '70px' as const,
-    mainContentWidth: '1100px' as const,
-    mainContentGap: '10rem' as const,
-    equipmentMargin: '6rem' as const,
-    equipmentGap: '4rem' as const,
+    navbarHeight: '70px',
+    mainContentWidth: '1100px',
+    mainContentGap: '10rem',
+    equipmentMargin: '6rem',
+    equipmentGap: '4rem',
   },
 
   up: (breakpoint: string, vertical = false) =>
@@ -55,11 +55,11 @@ const mainTheme = {
     }: calc(${breakpointMin} + 1px))`,
 
   breakpoints: {
-    l: '900px' as const,
-    m: '767px' as const,
-    s: '450px' as const,
+    l: '900px',
+    m: '767px',
+    s: '450px',
   },
-}
+} as const
 
 export type MainThemeType = typeof mainTheme
 export default mainTheme
